26977e8589d28af145ff486eddad83599988ff52
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2024-12-09 05:09:55 +0530

    Added anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,11 @@
 */
 
 function isAnagram(str1, str2) {
-
+    if(str1.length != str1.length) return false;
+    var str1A = str1.toLowerCase().split("").sort().join('');
+    var str2B = str2.toLowerCase().split("").sort().join('');
+    if(str1A==str2B) return true;
+    return false;
 }
 
 module.exports = isAnagram;
